828dd8835acbae566e997a15f009f2e15e95ad08
coreknowledge <coreknowledge666@gmail.com>
2020-06-10 17:28:10 +0800

    local p100b

--- machinePolicy/valueIteration.py
+++ machinePolicy/valueIteration.py
@@ -13,7 +13,7 @@ import pandas as pd
 import seaborn as sns
 
 # from viz import *
-# from reward import *
+from reward import *
 
 
 class GridWorld():
@@ -391,7 +391,7 @@ if __name__ == '__main__':
     for sheep_states in sheep_states_all:
         t += 1
         # sheep_states = ((6, 2), (6, 6))
-        sheep_states = ((7, 3), (3, 7))
+        # sheep_states = ((7, 3), (3, 7))
 
         # sheep_states = ((4, 2), (2, 4))
         print(sheep_states)
@@ -430,7 +430,7 @@ if __name__ == '__main__':
             distance_mean_reward, goal=sheep_states, unit=1)
 
 
-        costPerStep  = -10/(2*gridSize)
+        costPerStep  = -goalReward/(2*gridSize)
         grid_reward = ft.partial(
             grid_reward, env=env, const=costPerStep, terminals=sheep_states)
 
@@ -446,7 +446,7 @@ if __name__ == '__main__':
         R_arr = np.asarray([[[R[s][a].get(s_n, 0) for s_n in S]
                              for a in A] for s in S])
 
-        gamma = 0.9
+        gamma = 0.99
         value_iteration = ValueIteration(gamma, epsilon=0.0001, max_iter=100,terminals=sheep_states)
         V = value_iteration(S, A, T, R)
         V.update(terminalValue)
@@ -460,27 +460,27 @@ if __name__ == '__main__':
         Q_dict_output = {(s, sheep_states): {a: Q[si, ai] for (ai, a) in enumerate(A)} for (si, s) in enumerate(S)}
         # print (Q_dict_output)
 
-        normlizedQ_dict = {}
-        for wolf_state in S:
-            normlizedQ_dict[wolf_state] = {action: np.divide(Q_dict[wolf_state][action], np.sum(list(Q_dict[wolf_state].values()))) for action in A}
+        # normlizedQ_dict = {}
+        # for wolf_state in S:
+        #     normlizedQ_dict[wolf_state] = {action: np.divide(Q_dict[wolf_state][action], np.sum(list(Q_dict[wolf_state].values()))) for action in A}
 
 # # viz V
-        def calMaxDiff(Qlist):
-            diff = sorted(Qlist)[-1] - sorted(Qlist)[-2]
-            return diff
-        QValueDiff = {s: calMaxDiff(Q_dict[s].values()) for s in Q_dict.keys()}
-        normlizedQValueDiff = {s: calMaxDiff(normlizedQ_dict[s].values()) for s in normlizedQ_dict.keys()}
-        actionProbMax = {s: max(normlizedQ_dict[s].values()) for s in Q_dict.keys()}
+        # def calMaxDiff(Qlist):
+        #     diff = sorted(Qlist)[-1] - sorted(Qlist)[-2]
+        #     return diff
+        # QValueDiff = {s: calMaxDiff(Q_dict[s].values()) for s in Q_dict.keys()}
+        # normlizedQValueDiff = {s: calMaxDiff(normlizedQ_dict[s].values()) for s in normlizedQ_dict.keys()}
+        # actionProbMax = {s: max(normlizedQ_dict[s].values()) for s in Q_dict.keys()}
 
-        mapValue = 'V'
-        heatMapValue = eval(mapValue)
-        y = dict_to_array(heatMapValue)
-        y = y.reshape((gridSize, gridSize))
-        df = pd.DataFrame(y, columns=[x for x in range(gridSize)])
-        sns.heatmap(df, annot=True, fmt='.3f')
-        plt.title('{} for goal at {} noise={} goalReward={}'.format(mapValue, sheep_states, noise, goalReward))
-        plt.show()
-        break
+        # mapValue = 'V'
+        # heatMapValue = eval(mapValue)
+        # y = dict_to_array(heatMapValue)
+        # y = y.reshape((gridSize, gridSize))
+        # df = pd.DataFrame(y, columns=[x for x in range(gridSize)])
+        # sns.heatmap(df, annot=True, fmt='.3f')
+        # plt.title('{} for goal at {} noise={} goalReward={}'.format(mapValue, sheep_states, noise, goalReward))
+        # plt.show()
+        # break
 
 
 # viz Q
@@ -502,11 +502,11 @@ if __name__ == '__main__':
         # print (Q_dict[(3, 3)])
 
 # # save value
-    # print (len(Q_merge))
-    # dirName = os.path.dirname(os.path.abspath(__file__))
-    # prefix = 'noise' + str(noise) + 'commitArea' + 'Gird' + str(env.nx)
-    # pickle_dump_single_result(
-    #     dirc=dirName, prefix=prefix, name="policy", data=Q_merge)
+    print (len(Q_merge))
+    dirName = os.path.dirname(os.path.abspath(__file__))
+    prefix = 'numGoal'+str(numSheeps)+'noise' + str(noise) + 'commitArea' + 'Gird' + str(env.nx)+'reward'+str(goalReward)
+    pickle_dump_single_result(
+        dirc=dirName, prefix=prefix, name="policy", data=Q_merge)
 
-    # endTime = time.time()
-    # print("Time taken {} seconds".format((endTime - startTime)))
+    endTime = time.time()
+    print("Time taken {} seconds".format((endTime - startTime)))
